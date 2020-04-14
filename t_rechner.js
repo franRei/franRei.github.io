3f5f5ebfa4fc639e77ead556413b9ecb7e307ff5
franRei <franziska.reichardt88@gmail.com>
2020-04-14 19:35:31 +0200

    ja

--- t_rechner.js
+++ t_rechner.js
@@ -1,7 +1,14 @@
 "use strict"
 document.addEventListener("DOMContentLoaded", function () {
-
+	//gets buttons from html
 	var form = document.getElementById("calc"),
-	out =document.querySelector("#calc output"),
+	out = document.querySelector("#calc output"),
 	overwrite = true;
+
+	//unterdrückt submit und page reload
+	form.addEventListener("submit", function (ev) {
+		ev.preventDefault();
+		ev.stopPropagation();
+		return false;
+	})
 })
