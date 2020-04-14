31764d1779012e0032dc39826b1ec8d526b7d456
franRei <franziska.reichardt88@gmail.com>
2020-04-14 18:36:05 +0200

    First commit

---
+++ t_rechner.js
@@ -0,0 +1,7 @@
+"use strict"
+document.addEventListener("DOMContentLoaded", function () {
+
+	var form = document.getElementById("calc"),
+	out =document.querySelector("#calc output"),
+	overwrite = true;
+})
