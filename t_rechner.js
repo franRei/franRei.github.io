6004c4d2212933436628799c522f364a4ab7b303
franRei <franziska.reichardt88@gmail.com>
2020-04-15 11:01:04 +0200

    s

--- t_rechner.js
+++ t_rechner.js
@@ -1,170 +1,198 @@
-"use strict"
+"use strict";
 document.addEventListener("DOMContentLoaded", function () {
-	//gets buttons from html
+	if (!Array.prototype.includes) {
+		Array.prototype.includes = function includes(searchElement) {
+			if (this == null) {
+				throw new TypeError('this is null or undefined');
+			}
+			var object = Object(this),
+				length = object.length >>> 0;
+			if (length === 0) {
+				return false;
+			}
+			var start = arguments[1] >> 0,
+				key = start < 0 ? Math.max(start + length, 0) : start,
+				currentElement;
+			while (key < length) {
+				currentElement = object[key];
+				if (searchElement === currentElement || (searchElement !== searchElement &&
+						currentElement !== currentElement)) {
+					return true
+				}
+				key++;
+			}
+			return false;
+		};
+	}
+	if (!NodeList.prototype.forEach) NodeList.prototype.forEach = Array.prototype
+		.forEach;
 	var form = document.getElementById("calc"),
-	out = document.querySelector("#calc output"),
-	overwrite = true;
+		out = document.querySelector("#calc output"),
+		overwrite = true;
 
-	//unterdrückt submit und page reload
-	form.addEventListener("submit", function (ev) {
-		ev.preventDefault();
-		ev.stopPropagation();
-		return false;
-
-		//another comment
-		//just another
-	})
-})
-
-// button functionalities
-document.querySelectorAll("#calc button").forEach(function (b) { 
-	var c = b.textContent;
-
-	switch (c) {
-
-		case "9":
-		case "8":
-		case "7":
-		case "6":
-		case "5":
-		case "4":
-		case "3":
-		case "2":
-		case "1":
-		case "0":
-		case ".":
-			b.addEventListener("click", function () {
-				input(c);
-			});
-		break;
-
-		case "+":
-		case "-":
-		case "×":
-		case "÷":
-			b.addEventListener("click", function () {
-				operator(c);
-			});
-		break;
-
-		case "√":
-		case "x²":
-		case "ln":
-			b.addEventListener("click", function () {
-				extra(c);
-			});
-		break;
-
-		case "=":
-			b.addEventListener("click", result);
-		break;
-
-		case "C":
-			b.addEventListener("click", clear);
-		break;
-	}
-
-document.addEventListener("keypress", function (ev) {
-	//decimal point, ASCII von Komma und Punkt"x²"
-	if ([45, 46].includes(ev.charCode)) {
-		//, .
-		input(".");
-	}
-
-	//digits
-	if ([48, 49, 50, 51, 52, 53, 54, 55, 56, 57].includes(ev.charCode)) {
-		//ASCII für 0-9
-		input(ev.charCode - 48);
-	}
-	//operator
-	if ([42, 43, 45, 47].includes(ev.charCode)) {
-		//ASCII für operator *+-/
-		operator(
-			["x", "+", "-", "÷"][
-				[42, 43, 45, 47].indexOf(ev.charCode)
-			]
-		);
-	}
-
-	//result
-	if (ev.charCode == 61) {
-		//ASCII für =
-		result();
-	}
-
-	//clear
-	if ([67, 99].includes(ev.charCode)) {
-		//C, c
-		clear();
-	}
-
-	//log
-	if ([76, 108].includes(ev.charCode)) {
-		//L, l
-		extra("ln");
-	}
-
-	//root
-	if ([82, 114].includes(ev.charCode)) {
-		//R, r
-		extra("√");
-	}
-
-	//square
-	if ([83, 115].includes(ev.charCode)) {
-		//S, s
-		extra("x²");
-	}
-
-	//additional clear and results
-	switch (ev.code) {
-		case "Backspace":
-		case "Delete":
-			clear();
-		break;
-
-		case "Enter":
-		case "NumpadEnter":
-			result();
-		break;
-	}
-
-});
-
-function extra (type) {
-	switch (type) {
-		case "√":
-			out.textContent = Math.sqrt(result(true));
-		break;
-
-		case "x²":
-			out.textContent = Math.pow(result(true), 2);
-		break;
-
-		case "ln":
-			out.textContent = Math.log(result(true));
-		break;
-	}
-	overwrite = true;
-}
-
-function result (noDisplay) {
-	var input = out.textContent,
-		r = 0
-
-	input = input.replace(/x/g, "*").replace(/÷/g, "/");
-
-	input = input.replace(/[^0-9. +\-*\/]/g, "");
-
-	try {
-		r = eval(input);
-	} catch (e) {
-		r = 0;
-	}
-
-	if (noDisplay !== true) {
-		out.textContent = r;
+	function clear() {
+		out.textContent = 0;
 		overwrite = true;
 	}
-	return r;
-}
+
+	function extra(type) {
+		switch (type) {
+		case "√":
+			out.textContent = Math.sqrt(result(true));
+			break;
+		case "x²":
+			out.textContent = Math.pow(result(true), 2);
+			break;
+		case "ln":
+			out.textContent = Math.log(result(true));
+			break;
+		}
+		overwrite = true;
+	}
+
+	function input(c) {
+		// remove leading zero?
+		if (overwrite) {
+			out.textContent = (c == "." ? "0." : c);
+		} else {
+			out.textContent += c;
+		}
+		overwrite = false;
+	}
+
+	function operator(c) {
+		out.textContent += " " + c + " ";
+		overwrite = false;
+	}
+
+	function result(noDisplay) {
+			var input = out.textContent,
+				r = 0;
+			// replace × with * and ÷ with / for eval()
+			input = input.replace(/×/g, "*")
+				.replace(/÷/g, "/");
+			// remove anything else that is not allowed here
+			input = input.replace(/[^0-9. +\-*\/]/g, "");
+			try {
+				r = eval(input);
+			} catch (e) {
+				r = 0;
+			}
+			if (noDisplay !== true) {
+				out.textContent = r;
+				overwrite = true;
+			}
+			return r;
+		}
+		// initialize only if <output> is found
+	if (out) {
+		form.addEventListener("submit", function (ev) {
+			// prevent form submission and page reload
+			ev.preventDefault();
+			ev.stopPropagation();
+			return false;
+		});
+		// button functionalities
+		document.querySelectorAll("#calc button")
+			.forEach(function (b) {
+				var c = b.textContent;
+				switch (c) {
+				case "9":
+				case "8":
+				case "7":
+				case "6":
+				case "5":
+				case "4":
+				case "3":
+				case "2":
+				case "1":
+				case "0":
+				case ".":
+					b.addEventListener("click", function () {
+						input(c);
+					});
+					break;
+				case "+":
+				case "-":
+				case "×":
+				case "÷":
+					b.addEventListener("click", function () {
+						operator(c);
+					});
+					break;
+				case "√":
+				case "x²":
+				case "ln":
+					b.addEventListener("click", function () {
+						extra(c);
+					});
+					break;
+				case "=":
+					b.addEventListener("click", result);
+					break;
+				case "C":
+					b.addEventListener("click", clear);
+					break;
+				}
+			});
+		// keyboard support
+		document.addEventListener("keypress", function (ev) {
+			// decimal point
+			if ([44, 46].includes(ev.charCode)) {
+				// , .
+				input(".");
+			}
+			// digits
+			if ([48, 49, 50, 51, 52, 53, 54, 55, 56, 57].includes(ev.charCode)) {
+				// 0-9
+				input(ev.charCode - 48);
+			}
+			// operators
+			if ([42, 43, 45, 47].includes(ev.charCode)) {
+				// * + - /
+				operator(
+					["×", "+", "-", "÷"][
+						[42, 43, 45, 47].indexOf(ev.charCode)
+					]);
+			}
+			// result
+			if (ev.charCode == 61) {
+				// =
+				result();
+			}
+			// clear
+			if ([67, 99].includes(ev.charCode)) {
+				// C, c
+				clear();
+			}
+			// logarithm
+			if ([76, 108].includes(ev.charCode)) {
+				// L, l
+				extra("ln");
+			}
+			// root
+			if ([82, 114].includes(ev.charCode)) {
+				// R, r
+				extra("√");
+			}
+			// square
+			if ([83, 115].includes(ev.charCode)) {
+				// S, s
+				extra("x²");
+			}
+			// additional clear and result keys
+			switch (ev.code) {
+				// <delete> and <backspace> to clear display
+			case "Backspace":
+			case "Delete":
+				clear();
+				break;
+				// both <enter> keys to display result
+			case "Enter":
+			case "NumpadEnter":
+				result();
+				break;
+			}
+		});
+	}
+});
